d847820c174e8e391f298cc4acb2bc1f9735f9b6
Harish Saud <saudharish770@gmail.com>
2025-02-24 21:14:22 +0545

    Delete SingleNumber.py

--- SingleNumber.py
+++
@@ -1,9 +0,0 @@
-def single_number(nums):
-    result = 0
-    for num in nums:
-        result ^= num  # XOR operation cancels out duplicate numbers
-    return result
-
-# Example usage
-nums = [4, 1, 2, 1, 2]
-print(single_number(nums))  # Output: 4
