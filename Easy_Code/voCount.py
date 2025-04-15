1c728eaff1dd4ff1d3dc89687ff34abea6036664
Harish Saud <saudharish770@gmail.com>
2025-04-15 21:19:01 +0545

    Update voCount.py

--- Easy_Code/voCount.py
+++ Easy_Code/voCount.py
@@ -3,5 +3,5 @@ def count_vowels(s):
     return sum(1 for char in s if char in vowels)  # Count vowels in String
 
 # Example usage
-s = "Hello World"
+s = "Hello Worlds...."
 print(count_vowels(s))  # Output: 3 (e, o, o)
